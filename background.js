7dc55afbfc667802a7b241a063b3a618eda51b72
Matear890 <42663130+Matear890@users.noreply.github.com>
2026-05-26 23:49:28 +0800

    Delete background.js

--- background.js
+++
@@ -1,6 +0,0 @@
-// 报文转 Text - Background Script (minimal)
-// No active logic needed; all functionality is in popup.js
-
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("[报文转 Text] Extension installed");
-});
